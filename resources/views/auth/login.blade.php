93911e3efbcd33d6011424e44fbc23b35a9d6dbe
dickymahfudin <dickymahfudin13@gmail.com>
2020-03-28 17:43:47 +0700

    template login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,65 @@
-@extends('layouts.app')
+@extends('layouts.template')
 
 @section('content')
-<div class="container">
+
+<div class="limiter">
+    <div class="container-login100">
+        <div class="wrap-login100">
+            <div class="login100-pic js-tilt" data-tilt>
+                <img src="{{asset('images/img-01.png')}}" alt="IMG">
+            </div>
+
+            <form action="{{ route('login') }}" class="login100-form validate-form" method="POST">
+                @csrf
+                <span class="login100-form-title">
+                    Login
+                </span>
+                <div class="wrap-input100 validate-input">
+                    <input id="email" class="input100 @error('email') is-invalid @enderror" type="email" name="email" placeholder="Email" value="{{ old('email') }}" required autocomplete="email">
+                    <span class="focus-input100"></span>
+                    <span class="symbol-input100">
+                        <i class="fa fa-envelope" aria-hidden="true"></i>
+                    </span>
+
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+
+                <div class="wrap-input100 validate-input">
+                    <input id="password" class="input100 @error('password') is-invalid @enderror" type="password" name="password" placeholder="Password"  required autocomplete="new-password">
+                    <span class="focus-input100"></span>
+                    <span class="symbol-input100">
+                        <i class="fa fa-lock" aria-hidden="true"></i>
+                    </span>
+
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+
+                <div class="container-login100-form-btn">
+                    <button type="submit"  class="login100-form-btn">
+                        {{ __('Login') }}
+                    </button>
+
+                    @if (Route::has('password.request'))
+                        <a class="btn btn-link" href="{{ route('password.request') }}">
+                            {{ __('Forgot Your Password?') }}
+                        </a>
+                    @endif
+                </div>
+
+            </form>
+        </div>
+    </div>
+</div>
+
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -69,5 +127,5 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
 @endsection
