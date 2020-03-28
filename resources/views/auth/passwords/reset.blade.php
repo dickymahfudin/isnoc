cae7f032b4af0352519f34cd22d612fb330df17c
dickymahfudin <dickymahfudin13@gmail.com>
2020-03-28 17:53:17 +0700

    confirm password form

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,7 +1,70 @@
-@extends('layouts.app')
+@extends('layouts.template')
 
 @section('content')
-<div class="container">
+
+<div class="limiter">
+    <div class="container-login100">
+        <div class="wrap-login100">
+            <div class="login100-pic js-tilt" data-tilt>
+                <img src="{{asset('images/img-01.png')}}" alt="IMG">
+            </div>
+
+            <form action="{{ route('password.update') }}" class="login100-form validate-form" method="POST">
+                @csrf
+                <span class="login100-form-title">
+                    {{ __('Reset Password') }}
+                </span>
+
+                <input type="hidden" name="token" value="{{ $token }}">
+
+                <div class="wrap-input100 validate-input">
+                    <input id="email" class="input100 @error('email') is-invalid @enderror" type="email" name="email" placeholder="Email" value="{{ $email ?? old('email') }}" required autocomplete="email">
+                    <span class="focus-input100"></span>
+                    <span class="symbol-input100">
+                        <i class="fa fa-envelope" aria-hidden="true"></i>
+                    </span>
+
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+
+                <div class="wrap-input100 validate-input">
+                    <input id="password" class="input100 @error('password') is-invalid @enderror" type="password" name="password" placeholder="Password"  required autocomplete="new-password">
+                    <span class="focus-input100"></span>
+                    <span class="symbol-input100">
+                        <i class="fa fa-lock" aria-hidden="true"></i>
+                    </span>
+
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+
+                <div class="wrap-input100 validate-input">
+                    <input id="password-confirm" class="input100" type="password" name="password_confirmation" placeholder="Password Confirmation" required autocomplete="new-password">
+                    <span class="focus-input100"></span>
+                    <span class="symbol-input100">
+                        <i class="fa fa-lock" aria-hidden="true"></i>
+                    </span>
+                </div>
+
+                <div class="container-login100-form-btn">
+                    <button type="submit"  class="login100-form-btn">
+                        {{ __('Reset Password') }}
+                    </button>
+                </div>
+
+            </form>
+        </div>
+    </div>
+</div>
+
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -61,5 +124,5 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
 @endsection
