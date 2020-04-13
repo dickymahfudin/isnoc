a329261f0be22112626594a20dc9625a0eeb16b9
dickymahfudin <dickymahfudin13@gmail.com>
2020-04-13 13:45:25 +0700

    midleware edit

--- routes/web.php
+++ routes/web.php
@@ -21,11 +21,11 @@ use Illuminate\Auth\Events\Verified;
 Auth::routes(['verify' => true]);
 
 Route::redirect('/', '/home', 301);
-Route::redirect('/register', '/home', 301);
+// Route::redirect('/register', '/home', 301);
 
-Route::get('/home', 'NocController@index')->name('noc')->middleware('verified');
+Route::get('/home', 'NocController@index')->name('noc');
 
-Route::group(['prefix' => 'nojs',  'middleware' => 'verified'], function () {
+Route::group(['prefix' => 'nojs'], function () {
     Route::get('/', 'NojsUsersController@index')->name('nojs.index');
     Route::get('/table', 'NojsUsersController@dataTable')->name('nojs.table');
     Route::get('/detail', function () {
@@ -42,4 +42,4 @@ Route::group(['prefix' => 'nojs',  'middleware' => 'verified'], function () {
 
 Route::get('/servicecalls', function () {
     return view('servicecalls.index');
-})->name('servicecalls')->middleware('verified');
+})->name('servicecalls');
