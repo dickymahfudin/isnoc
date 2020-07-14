e1c4b787b44864a59f4c11329ee553c01a0524c1
dickymahfudin <dickymahfudin13@gmail.com>
2020-07-14 11:01:58 +0700

    pg_dump path

--- config/backup.php
+++ config/backup.php
@@ -73,7 +73,7 @@ return [
              * For a complete list of available customization options, see https://github.com/spatie/db-dumper
              */
             'databases' => [
-                'mysql',
+                'pgsql',
             ],
         ],
 
--- config/database.php
+++ config/database.php
@@ -76,6 +76,11 @@ return [
             'prefix_indexes' => true,
             'schema' => 'public',
             'sslmode' => 'prefer',
+            'dump' => [  // Added
+                'dump_binary_path' => env('PGSQL_DUMP_PATH'),
+                'use_single_transaction',
+                'timeout' => 60 * 100000,
+            ]
         ],
 
         'sqlsrv' => [
@@ -123,7 +128,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
