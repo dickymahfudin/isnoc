57f2a4ff9e32f30aeb80e0a66d74d60eab583331
dickymahfudin <dickymahfudin13@gmail.com>
2020-07-29 15:30:27 +0700

    Artisan command send email SLA

---
+++ app/Console/Commands/EmailSlaMonthly.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
+use App\Http\Controllers\Api\SlaPrtgController;
+use App\Exports\SlaMonthlyMultipleSheet;
+use App\Mail\SlaPrtgMonthlyMail;
+use Maatwebsite\Excel\Facades\Excel;
+use Illuminate\Support\Facades\Mail;
+
+class EmailSlaMonthly extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'email:month';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Get Sla Prtg Monthly and send email';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        Storage::delete('sla.xls');
+        $sla = SlaPrtgController::monthly();
+        $data = $sla->getOriginalContent();
+        // dd($data);
+        // dd($data["sla_prtg"]);
+        Excel::store(new SlaMonthlyMultipleSheet($data), 'sla.xls');
+
+        $to = ['pungki@sundaya.com', 'malik@sundaya.com'];
+        $cc = ['dicky@sundaya.com'];
+
+        Mail::to($to)
+            ->cc($cc)
+            ->send(new SlaPrtgMonthlyMail($data));
+    }
+}
